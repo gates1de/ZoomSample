159d57bfc8123efe311a12583e251decca384a44
gates1de <g031i043@gmail.com>
2018-06-13 06:58:52 +0900

    :page_facing_up: Create project

---
+++ ZoomSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZoomSample
+//
+//  Created by Yu Kadowaki on 2018/06/13.
+//  Copyright © 2018 gates1de. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
