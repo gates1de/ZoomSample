d88a67a4ea641c29f18eedde9e8b831298521fa4
gates1de <g031i043@gmail.com>
2018-06-16 14:37:39 +0900

    :books: Add MobileRTC framework

---
+++ ZoomSample/ZoomSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ZoomSample-Bridging-Header.h
+//  ZoomSample
+//
+//  Created by Yu Kadowaki on 2018/06/16.
+//  Copyright © 2018 gates1de. All rights reserved.
+//
+
+#ifndef ZoomSample_Bridging_Header_h
+#define ZoomSample_Bridging_Header_h
+
+#import <MobileRTC/MobileRTC.h>
+
+#endif /* ZoomSample_Bridging_Header_h */
